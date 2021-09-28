2e7860375c3690672762a5a501cb104f5154340e
Artyom Vasilyev <metagxd@gmail.com>
2021-09-28 12:55:22 +0300

    imports w/o '*'

--- src/main/java/com/metagxd/solvotest/repository/LocationRepositoryImpl.java
+++ src/main/java/com/metagxd/solvotest/repository/LocationRepositoryImpl.java
@@ -6,8 +6,16 @@ import com.metagxd.solvotest.util.DbUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.*;
-import java.util.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class LocationRepositoryImpl implements LocationRepository {
 
